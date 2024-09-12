157f10cc73755690b70666c128f57247ca4988f3
tulam120604 <saudoi1204@gmail.com>
2024-09-12 17:32:25 +0700

    12/9

--- src/app/(Client)/cart/_components/page.cart.tsx
+++ src/app/(Client)/cart/_components/page.cart.tsx
@@ -2,7 +2,7 @@
 'use client';
 
 import React, { Suspense, useEffect, useState } from 'react'
-import LoadingCart from './loading';
+import LoadingCart from '../loading';
 import { Get_Items_Cart } from '@/src/app/_lib/Tanstack_Query/Cart/query';
 import { useRouter } from 'next/navigation';
 import { Mutation_Cart } from '@/src/app/_lib/Tanstack_Query/Cart/mutation_Cart';
@@ -10,9 +10,10 @@ import { io } from 'socket.io-client';
 import { useToast } from '@/src/app/_Components/ui/use-toast';
 import { ToastAction } from '@/src/app/_Components/ui/toast';
 import { useCheck_user } from '@/src/app/_lib/Custome_Hooks/User';
-import Table_Cart from './_components/table';
+import Table_Cart from './table';
 import { Button } from '@/src/app/_Components/ui/Shadcn/button';
 import Breadcrum from '@/src/app/_Components/breadcrum/breadcrum';
+import { filter_positive_Stock_Item } from '@/src/app/_lib/Config/Filter_Cart_And_Order';
 
 const Cart = () => {
   const { toast } = useToast();
@@ -29,7 +30,7 @@ const Cart = () => {
         ),
       })
     })
-  }, [])
+  }, []);
 
   const routing = useRouter();
   const { mutate } = Mutation_Cart("CHECKED_AND_REMOVE_ALL");
@@ -74,10 +75,13 @@ const Cart = () => {
   }
 
   const data_item_checkked = data?.items?.filter((item: any) => (item?.status_checked && item));
+  // lọc item só lượng lớn hơn 0
+  const positive_Stock_Item = filter_positive_Stock_Item(data_checked_true);
+  const tota_price_item = positive_Stock_Item?.reduce((acc: any, curr: any) => (acc + curr?.total_price_item), 0);
   function showToast(productName: string | number, stock: string | number) {
     toast({
       title: "Thông báo!",
-      description: `Rất tiếc, sản phẩm ${productName} chỉ còn ${stock} chiếc. Vui lòng giảm số lượng thanh toán!`,
+      description: ` ${(+stock > 0) ? `Rất tiếc, sản phẩm ${productName} chỉ còn ${stock} chiếc. Vui lòng giảm số lượng thanh toán!` : `Rất tiếc, sản phẩm ${productName} đã hết hàng!`}`,
       className: 'border border-gray-800',
       action: (
         <ToastAction altText="Goto schedule to undo">Ok</ToastAction>
@@ -87,31 +91,36 @@ const Cart = () => {
   // next order
   function next_page_payment() {
     // check so luong
-    for (let i of data_item_checkked) {
+    for (let i of positive_Stock_Item) {
       if (i?.product_id?.attributes) {
-        for (let j of i?.product_id?.attributes?.varriants) {
-          for (let k of j?.size_item) {
-            if (i?.size_attribute_item) {
-              if (i?.color_item === j?.color_item && i?.quantity > k?.stock_item && i?.size_attribute_item === k?.name_size) {
-                showToast(i?.product_id?.short_name, k?.stock_item)
-                return null;
-              }
-            }
-            if (i?.color_item === j?.color_item && i?.quantity > k?.stock_item) {
-              showToast(i?.product_id?.short_name, k?.stock_item)
-              return null;
-            }
-          }
+        const check_color = i?.product_id?.attributes?.varriants?.find((value: any) => value?.color_item === i?.color_item);
+        const check_size = check_color?.size_item?.find((value: any) =>
+          i?.size_attribute_item === (value?.name_size?.trim() ? value?.name_size : undefined));
+        if (check_color?.color_item === i?.color_item && check_size?.name_size === i?.size_attribute_item
+          && i?.quantity > check_size?.stock_item) {
+          showToast(i?.product_id?.short_name, check_size?.stock_item);
+          return null
+        }
+        if (check_color?.color_item === i?.color_item && i?.quantity > check_size?.stock_item) {
+          showToast(i?.product_id?.short_name, check_size?.stock_item);
+          return null
         }
       }
-      else {
-        if (i?.quantity > i?.product_id?.stock) {
-          showToast(i?.product_id?.short_name, i?.product_id?.stock)
-          return null;
-        }
+      else if (i?.quantity > i?.product_id?.stock) {
+        showToast(i?.product_id?.short_name, i?.product_id?.stock)
+        return null;
       }
     }
-    routing.push('/checkout');
+    if (data_item_checkked?.length > 0) {
+      routing.push('/checkout');
+    }
+    else {
+      toast({
+        description: `Vui lòng chọn sản phẩm để tiến hành thanh toán!`,
+        className: 'border border-gray-800',
+        duration: 1000,
+      });
+    }
   }
   // console.count('re-render : ')
   const dataProps = {
@@ -125,23 +134,21 @@ const Cart = () => {
   return (
     <Suspense fallback={<LoadingCart />}>
       <div className="max-w-[1440px] w-[95vw] mx-auto pb-8">
-        <div className='max-w-[1440px] mx-auto w-[95vw] mx-auto mb-4 mt-2'>
+        <div className='max-w-[1440px] mx-auto w-[95vw] mx-auto mb-4 pt-2'>
           <Breadcrum textProps={{ name_item: 'Giỏ hàng' }} />
         </div>
         {/* list items */}
         <Table_Cart dataProps={dataProps} />
-
-        <div className="w-full rounded-lg lg:flex items-center justify-between bg-white py-2 px-4 lg:p-4 gap-x-4 sticky bottom-0 z-[10] shadow-[0_-5px_20px_-15px_rgba(0,0,0,0.3)] mt-8">
+        <div className="w-full rounded-lg flex flex-col lg:flex-row items-center lg:justify-between justify-center bg-white py-2 px-4 lg:p-4 gap-x-4 sticky bottom-0 z-[10] shadow-[0_-5px_20px_-15px_rgba(0,0,0,0.3)] mt-8">
           <span className="text-gray-800 whitespace-nowrap text-sm lg:text-base">Số lượng ({data_item_checkked?.length} sản phẩm)</span>
           <Button onClick={next_page_payment} type='button' className="flex gap-x-4 mt-2 lg:mt-0">
             <span>Tiến hành thanh toán</span>
             |
-            <span>{data?.total_price?.toLocaleString('vi', { style: 'currency', currency: 'VND' })}</span>
+            <span>{tota_price_item?.toLocaleString('vi', { style: 'currency', currency: 'VND' })}</span>
           </Button>
         </div>
       </div>
     </Suspense >
   )
 }
-
 export default Cart
--- src/app/Zustand/Store.ts
+++ src/app/Zustand/Store.ts
@@ -1,22 +1,29 @@
-// import { create } from 'zustand';
-// import { IDataPayment } from '../Types/Interface';
+import { create } from 'zustand';
+import { IDataPayment } from '../Types/Interface';
 
-// interface IStoreZustand {
-//     data: any[],
-//     isLoading: boolean,
-//     isError: boolean,
-//     setData: (value: IDataPayment | null) => void,
-//     setLoading: (loading: any) => void,
-//     setError: (error: any) => void,
-// }
+interface IStoreZustand {
+    isVisible: any
+    data: any[],
+    isLoading: boolean,
+    isError: boolean,
+    setVisible: (value: any) => void,
+    setData: (value: IDataPayment | null) => void,
+    setLoading: (loading: any) => void,
+    setError: (error: any) => void,
+}
 
-// const useStoreZustand = create<IStoreZustand>((set) => ({
-//     data: [],
-//     isLoading: false,
-//     isError: false,
-//     setData: ((values: any) => set({ data: values })),
-//     setLoading: ((loading: boolean) => set({ isLoading: loading })),
-//     setError: ((error: boolean) => set({ isError: error }))
-// }))
+const useStoreZustand = create<IStoreZustand>((set) => ({
+    isVisible: undefined,
+    data: [],
+    isLoading: false,
+    isError: false,
+    setVisible: ((value: any) => {
+        set({ isVisible: value });
+        setTimeout(() => set({ isVisible: undefined }), 1500)
+    }),
+    setData: ((values: any) => set({ data: values })),
+    setLoading: ((loading: boolean) => set({ isLoading: loading })),
+    setError: ((error: boolean) => set({ isError: error }))
+}))
 
-// export default useStoreZustand
+export default useStoreZustand
