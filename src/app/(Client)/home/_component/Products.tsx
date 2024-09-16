c1815b4352e97ace2d89d38443a4d7ffd86b2996
tulam120604 <saudoi1204@gmail.com>
2024-09-16 21:23:28 +0700

    update 16/9

--- src/app/(Client)/home/_component/Products.tsx
+++ src/app/(Client)/home/_component/Products.tsx
@@ -2,12 +2,12 @@ import Link from 'next/link';
 import React from 'react'
 import List_Products from '../../../_Components/Products/List_Products';
 import Loading_Dots from '@/src/app/_Components/Loadings/Loading_Dots';
-import { getLimit } from '@/src/app/_lib/Services/Services_Items/products';
 import { unstable_noStore as noStore } from 'next/cache';
+import { GET_limit_item } from '@/src/app/_lib/Services/Services_Items/products';
 
 const Products_Home = async () => {
     noStore();
-    const data = await getLimit(42);
+    const data = await GET_limit_item(42);
     //   const isClient = typeof window !== 'undefined';
     // console.log(isClient);
     return (
@@ -20,9 +20,8 @@ const Products_Home = async () => {
                 </div>
             </div></>) :
                 (<>
-                    <div className="mx-auto max-w-[1440px] md:w-[90vw] w-[342px] relative text-center mb-4 lg:mb-8">
-                        <strong className="relative z-[2] text-[gray-800 font-medium bg-[#F5F5FA] lg:text-xl px-4 mb:text-lg">GỢI Ý HÔM NAY</strong>
-                        <div className="absolute w-full h-[1px] bg-gray-400 top-1/2 z-[1]"></div>
+                    <div className="mx-auto max-w-[1440px] md:w-[90vw] w-[342px] relative text-center mb-4 lg:mb-8 bg-white py-4 border-b-4 border-gray-700">
+                        <strong className="relative z-[2] text-[gray-800 font-medium lg:text-lg px-4">GỢI Ý HÔM NAY</strong>
                     </div>
                     {
                         data?.data?.docs ?
