5b8424716f6c599c74e5dd3af049fd5ecdcbc98c
tulam120604 <saudoi1204@gmail.com>
2025-09-09 10:39:18 +0700

    update

--- next.config.ts
+++ next.config.ts
@@ -1,21 +1,29 @@
 module.exports = {
-    async redirects() {
-      return [
-        // Basic redirect
-        {
-          source: '/',
-          destination: '/products',
-          permanent: true,
-        },
-        // Wildcard path matching
-        {
-          source: '/blog/:slug',
-          destination: '/news/:slug',
-          permanent: true,
-        },
-      ]
-    },
-    experimental : {
-      appDir : true
-    }
-  }
+  async redirects() {
+    return [
+      // Basic redirect
+      {
+        source: "/",
+        destination: "/products",
+        permanent: true,
+      },
+      // Wildcard path matching
+      {
+        source: "/blog/:slug",
+        destination: "/news/:slug",
+        permanent: true,
+      },
+    ];
+  },
+  async rewrite() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "https://be-node-js.vercel.app/v1/:path*",
+      },
+    ];
+  },
+  experimental: {
+    appDir: true,
+  },
+};
