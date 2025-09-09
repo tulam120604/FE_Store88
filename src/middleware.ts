4f6112d1ed90e06e181f193b5c2f736fc2543e7d
tulam120604 <saudoi1204@gmail.com>
2025-09-09 13:54:31 +0700

    test deploy

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,73 +1,73 @@
-import { NextResponse } from "next/server";
-import type { NextRequest } from "next/server";
-import { jwtVerify } from "jose";
+// import { NextResponse } from "next/server";
+// import type { NextRequest } from "next/server";
+// import { jwtVerify } from "jose";
 
-// verify jwt
-const secretJWT = new TextEncoder().encode(process.env.NEXT_PUBLIC_JWT_SECRET);
-const allowRole = ["admin_global", "admin_local", "seller"];
-const verifyToken = async (token: string) => {
-  try {
-    const secret = await jwtVerify(String(token), secretJWT);
-    return secret;
-  } catch (error) {
-    console.error("JWT verify error:", error);
-    return null;
-  }
-};
+// // verify jwt
+// const secretJWT = new TextEncoder().encode(process.env.NEXT_PUBLIC_JWT_SECRET);
+// const allowRole = ["admin_global", "admin_local", "seller"];
+// const verifyToken = async (token: string) => {
+//   try {
+//     const secret = await jwtVerify(String(token), secretJWT);
+//     return secret;
+//   } catch (error) {
+//     console.error("JWT verify error:", error);
+//     return null;
+//   }
+// };
 
-export async function middleware(request: NextRequest) {
-  const url = request.nextUrl;
-  const pathname = url.pathname;
-  const tokenCookie = request.cookies.getAll();
-  console.log(tokenCookie);
+// export async function middleware(request: NextRequest) {
+//   const url = request.nextUrl;
+//   const pathname = url.pathname;
+//   const tokenCookie = request.cookies.getAll();
+//   console.log(tokenCookie);
 
-  const token = request.cookies.get("access_token")?.value || "";
+//   const token = request.cookies.get("access_token")?.value || "";
 
-  const verifyTokenResult: any = await verifyToken(String(token));
-  if (!verifyTokenResult) {
-    if (pathname.startsWith("/thong-tin-tai-khoan")) {
-      return NextResponse.redirect(new URL("/", request.url));
-    }
-  }
+//   const verifyTokenResult: any = await verifyToken(String(token));
+//   if (!verifyTokenResult) {
+//     if (pathname.startsWith("/thong-tin-tai-khoan")) {
+//       return NextResponse.redirect(new URL("/", request.url));
+//     }
+//   }
 
-  const role = verifyTokenResult?.payload?.role;
-  const userId = verifyTokenResult?.payload?.userId;
+//   const role = verifyTokenResult?.payload?.role;
+//   const userId = verifyTokenResult?.payload?.userId;
 
-  // user đã login thì chặn vào /tai-khoan
-  if (userId && pathname.startsWith("/tai-khoan")) {
-    return NextResponse.redirect(new URL("/", request.url));
-  }
+//   // user đã login thì chặn vào /tai-khoan
+//   if (userId && pathname.startsWith("/tai-khoan")) {
+//     return NextResponse.redirect(new URL("/", request.url));
+//   }
 
-  // role cho /trung-tam-dieu-khien
-  console.log(pathname);
-  if (pathname.startsWith("/trung-tam-dieu-khien")) {
-    if (!role || !allowRole.includes(role)) {
-      return NextResponse.redirect(new URL("/", request.url));
-    }
+//   // role cho /trung-tam-dieu-khien
+//   console.log(pathname);
+//   if (pathname.startsWith("/trung-tam-dieu-khien")) {
+//     if (!role || !allowRole.includes(role)) {
+//       return NextResponse.redirect(new URL("/", request.url));
+//     }
 
-    const sellerAllowPaths = [
-      "/trung-tam-dieu-khien/bang-dieu-khien",
-      "/trung-tam-dieu-khien/san-pham",
-      "/trung-tam-dieu-khien/don-hang",
-    ];
+//     const sellerAllowPaths = [
+//       "/trung-tam-dieu-khien/bang-dieu-khien",
+//       "/trung-tam-dieu-khien/san-pham",
+//       "/trung-tam-dieu-khien/don-hang",
+//     ];
 
-    if (
-      role === "seller" &&
-      !sellerAllowPaths?.some((p) => pathname.startsWith(p))
-    ) {
-      return NextResponse.redirect(
-        new URL("/trung-tam-dieu-khien/bang-dieu-khien/tong-quan", request.url)
-      );
-    }
-  }
+//     if (
+//       role === "seller" &&
+//       !sellerAllowPaths?.some((p) => pathname.startsWith(p))
+//     ) {
+//       return NextResponse.redirect(
+//         new URL("/trung-tam-dieu-khien/bang-dieu-khien/tong-quan", request.url)
+//       );
+//     }
+//   }
 
-  return NextResponse.next();
-}
+//   return NextResponse.next();
+// }
 
-export const config = {
-  matcher: [
-    "/tai-khoan",
-    "/thong-tin-tai-khoan",
-    "/trung-tam-dieu-khien/:path*",
-  ],
-};
+// export const config = {
+//   matcher: [
+//     "/tai-khoan",
+//     "/thong-tin-tai-khoan",
+//     "/trung-tam-dieu-khien/:path*",
+//   ],
+// };
