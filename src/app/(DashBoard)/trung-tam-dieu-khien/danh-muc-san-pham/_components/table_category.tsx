ebb734b529fe247b2a5a770e1c0b0b4bbbbfb973
tulam120604 <saudoi1204@gmail.com>
2025-09-17 23:26:23 +0700

    update

--- src/app/(DashBoard)/trung-tam-dieu-khien/danh-muc-san-pham/_components/table_category.tsx
+++ src/app/(DashBoard)/trung-tam-dieu-khien/danh-muc-san-pham/_components/table_category.tsx
@@ -16,28 +16,28 @@ export default function CategoryTable({ data }: any) {
   return (
     <Table className="rounded border bg-white dark:bg-[#0F1629] overflow-hidden">
       <TableHeader>
-        <TableRow className="*:font-semibold opacity-90 *:border-none">
+        <TableRow className="*:font-semibold opacity-90 *:border-none text-center">
           <TableHead>Ảnh</TableHead>
           <TableHead>Tên danh mục</TableHead>
           <TableHead>Số sản phẩm</TableHead>
-          {/* <TableHead>Trạng thái</TableHead> */}
-          <TableHead className="hidden lg:block">Ngày tạo/cập nhật</TableHead>
-          <TableHead className="text-right">Thao tác</TableHead>
+          <TableHead>Ngày tạo/cập nhật</TableHead>
+          {/* <TableHead className="text-right">Thao tác</TableHead> */}
         </TableRow>
       </TableHeader>
       <TableBody>
         {data?.map((item: any, idx: number) => (
-          <TableRow key={idx} className="*:border-gray-200 *:dark:border-gray-700">
+          <TableRow
+            key={idx}
+            className="*:border-gray-200 *:dark:border-gray-700"
+          >
             <TableCell>
-              <div>
-                <Image
-                  className="w-12 h-12 relative overflow-hidden object-cover"
-                  src={item?.category_img}
-                  alt={item?.name}
-                  height={60}
-                  width={60}
-                />
-              </div>
+              <Image
+                className="w-12 h-12 relative overflow-hidden object-cover"
+                src={item?.category_img}
+                alt={item?.name}
+                height={60}
+                width={60}
+              />
             </TableCell>
             <TableCell className="font-medium">{item?.category_name}</TableCell>
             <TableCell>{item?.countProduct}</TableCell>
@@ -52,15 +52,17 @@ export default function CategoryTable({ data }: any) {
                 {item.status}
               </span>
             </TableCell> */}
-            <TableCell className="hidden lg:block">{item.updatedAt?.slice(0, 10)}</TableCell>
-            <TableCell className="flex justify-end gap-2 *:border-none">
+            <TableCell>
+              {item.updatedAt?.slice(0, 10)}
+            </TableCell>
+            {/* <TableCell className="*:border-none">
               <Button size="icon" variant="outline">
                 <FilePenLine className="h-5 text-gray-700" />
               </Button>
-              {/* <Button size="icon" variant="destructive">
+              <Button size="icon" variant="destructive">
                 <Trash2 className="w-4 h-4" />
-              </Button> */}
-            </TableCell>
+              </Button>
+            </TableCell> */}
           </TableRow>
         ))}
       </TableBody>
